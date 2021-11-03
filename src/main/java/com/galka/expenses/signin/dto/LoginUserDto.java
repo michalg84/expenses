daff01e9605f2792ff6642816daeed6f74e13118
michał gałka <michalgalka84@gmail.com>
2021-11-03 15:12:58 +0100

    add login dto

---
+++ src/main/java/com/galka/expenses/signin/dto/LoginUserDto.java
@@ -0,0 +1,23 @@
+package com.galka.expenses.signin.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class LoginUserDto {
+
+    @NotEmpty(message = "Please insert valid username")
+    @Size(min = 5, max = 25, message = "5 - 25 characters are required !")
+    private String username;
+    @NotEmpty
+    @Size(min = 5, max = 25, message = "5 - 25 characters are required !")
+    private String password;
+}
