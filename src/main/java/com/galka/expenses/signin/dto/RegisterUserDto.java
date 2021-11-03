f6cbc7dc531bb03ef33305181aec9d150b7640f9
michał gałka <michalgalka84@gmail.com>
2021-11-03 15:13:10 +0100

    add register dto

---
+++ src/main/java/com/galka/expenses/signin/dto/RegisterUserDto.java
@@ -0,0 +1,30 @@
+package com.galka.expenses.signin.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class RegisterUserDto {
+
+    @NotEmpty(message = "Please insert valid username")
+    @Size(min = 5, max = 25, message = "5 - 25 characters are required !")
+    private String username;
+    @NotEmpty
+    @Email
+    private String mail;
+    @NotEmpty
+    @Size(min = 5, max = 25, message = "5 - 25 characters are required !")
+    private String password;
+    @NotEmpty
+    @Size(min = 5, max = 25, message = "5 - 25 characters are required !")
+    private String confirmPassword;
+}
